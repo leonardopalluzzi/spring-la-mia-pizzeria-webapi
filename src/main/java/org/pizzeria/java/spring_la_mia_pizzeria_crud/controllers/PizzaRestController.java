da2d2545fb9cb8a3b838b7694460522a0d4a4d61
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-07-11 13:42:50 +0200

    add rest controller with crud operations and tested with postman

--- src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
+++ src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
@@ -1,8 +1,12 @@
 package org.pizzeria.java.spring_la_mia_pizzeria_crud.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.pizzeria.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.pizzeria.java.spring_la_mia_pizzeria_crud.services.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,8 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,32 +24,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v2/pizza")
 public class PizzaRestController {
 
-    @Autowired
+    @Autowired 
     private PizzaService pizzaService;
 
-    @GetMapping("/")
-    public ResponseEntity<Pizza> index() {
-        ResponseEntity<Pizza> = pizzaService.findAll();
-
+    @GetMapping
+    public ResponseEntity<List<Pizza>> index(@RequestParam(name = "title", required = false) String title) {
+        List<Pizza> result = pizzaService.findAll();
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Pizza> show(@PathVariable Integer id) {
 
+        Optional<Pizza> result = pizzaService.findById(id);
+
+        if(result.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(result.get(), HttpStatus.OK);
+
     }
 
     @PostMapping("/create")
-    public void store(@ResponseBody pizza) {
+    public ResponseEntity<Pizza> store(@RequestBody Pizza pizza) {
+        return new ResponseEntity<Pizza>(pizzaService.save(pizza), HttpStatus.OK);
 
     }
 
-    @PutMapping("edit")
-    public void edit(@ResponseBody pizza){
+    @PutMapping("/edit")
+    public ResponseEntity<Pizza> edit(@RequestBody Pizza pizza){
+        return new ResponseEntity<Pizza>(pizzaService.save(pizza), HttpStatus.OK);
 
     }
 
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Integer id) {
+    public ResponseEntity<Pizza> delete(@PathVariable Integer id) {
+        Optional<Pizza> pizza = pizzaService.findById(id);
+
+        if(pizza.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        pizzaService.delete(pizza.get());
+        return new ResponseEntity<>(HttpStatus.OK);
 
     }
 
--- src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/services/PizzaService.java
+++ src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/services/PizzaService.java
@@ -1,6 +1,7 @@
 package org.pizzeria.java.spring_la_mia_pizzeria_crud.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.pizzeria.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.pizzeria.java.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
@@ -25,15 +26,20 @@ public class PizzaService {
         return pizzaList;
     }
 
-    public Pizza findById(Integer id) {
-        return pizzaRepo.findById(id).get();
+    public Optional<Pizza> findById(Integer id) {
+        return pizzaRepo.findById(id);
     }
 
-    public void save(Pizza pizza) {
-        pizzaRepo.save(pizza);
+    public Pizza save(Pizza pizza) {
+        return pizzaRepo.save(pizza);
     }
 
     public void delete(Pizza pizza) {
+        pizzaRepo.delete(pizza);
+    }
+
+    public void deleteById(Integer id) {
+        Pizza pizza = pizzaRepo.findById(id).get();
         pizzaRepo.delete(pizza);
     }
 
